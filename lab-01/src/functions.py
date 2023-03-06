07e5a57275d0e1bfec0ec12cb0d5d2725934a57a
Oleg-Dainovich <olegdainovich@gmail.com>
2023-03-06 22:45:38 +0300

    add calculator_method

---
+++ lab-01/src/functions.py
@@ -0,0 +1,9 @@
+def calculator_method (first_number, second_number, operation):
+    if (operation == "add"):
+        return first_number + second_number
+    elif (operation == "sub"):
+        return first_number - second_number
+    elif (operation == "mult"):
+        return first_number * second_number
+    elif (operation == "div"):
+        return first_number / second_number
--- lab-01/src/main.py
+++ lab-01/src/main.py
@@ -1,1 +1,11 @@
+from functions import *
+
 print("Hello, World!")
+
+first_num = input()
+second_num = input()
+operation = input()
+try:
+    calculator_method (first_num, second_num, operation)
+except ZeroDivisionError:
+    print("Division by zero!")

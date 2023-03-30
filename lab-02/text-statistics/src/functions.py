d93365a4919bfaa1f436fb6fbebb38ec7e3f26a7
Oleg-Dainovich <olegdainovich@gmail.com>
2023-03-31 01:28:18 +0300

    add non-declarative sentences counte in text statistics

--- lab-02/text-statistics/src/functions.py
+++ lab-02/text-statistics/src/functions.py
@@ -3,3 +3,7 @@ import re
 def count_sentences(text: str):
     reg_expr = r'(\.|\?|!)+'
     return len(re.findall(reg_expr, text))
+
+def count_non_declarative_sentences(text: str):
+    reg_expr = r'\.+'
+    return count_sentences(text) - len(re.findall(reg_expr, text))
--- lab-02/text-statistics/src/main.py
+++ lab-02/text-statistics/src/main.py
@@ -1,5 +1,6 @@
-from functions import count_sentences
+from functions import count_sentences, count_non_declarative_sentences
 
 text = input('Enter the text: ')
 
 print('Amount of sentences in the text: ', count_sentences(text))
+print('Amount of non-declarative sentences in the text: ', count_non_declarative_sentences(text))

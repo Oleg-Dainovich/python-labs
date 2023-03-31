411c08095979f9d2ca0fb694897366fddc33d6e2
Oleg-Dainovich <olegdainovich@gmail.com>
2023-04-01 01:06:24 +0300

    change function replace_abbreviations

--- lab-02/text-statistics/src/functions.py
+++ lab-02/text-statistics/src/functions.py
@@ -6,17 +6,23 @@ ABBREVIATIONS = (
     'jan.', 'feb.', 'mar.', 'jun.', 'jul.', 'aug.', 'sep.',
     'oct.', 'nov.', 'dec.', 'mon.', 'tue.', 'wed.', 'thu.',
     'fri.', 'sat.', 'sun.', 'mr.', 'mrs.', 'ms.', 'dr.',
-    'st.', 'inc.', 'corp.'  
+    'st.', 'inc.', 'corp.', 'in.', 'lbs.', 'e.g.', 'i.e.'  
 )
 
 def replace_abbreviations(text: str):
     new_text = text
     for abbreviation in ABBREVIATIONS:
-        new_text = re.sub(abbreviation, 'word', new_text)
+        new_text = re.sub(abbreviation, re.sub(r'\.', " ", abbreviation), new_text)
+    return new_text
+
+def find_words(text: str):
+    words_list = re.findall(r'\w*[a-zA-Z]\w*', text)
+    new_text = "".join(words_list)
     return new_text
 
 def count_sentences(text: str):
     new_text = replace_abbreviations(text)
+    print(new_text)
     reg_expr = r'(\.|\?|!)+'
     return len(re.findall(reg_expr, new_text))
 
@@ -27,6 +33,9 @@ def count_non_declarative_sentences(text: str):
  
 def count_length_of_sentences(text: str):
     new_text = replace_abbreviations(text)
+    print(new_text)
+    #new_text = find_words(new_text)
+    #print(new_text)
     reg_expr = r'(\w)+'
     return len(re.findall(reg_expr, new_text)) / count_sentences(text)
 
@@ -34,3 +43,5 @@ def count_length_of_words(text: str):
     reg_expr_letters = r'\w'
     reg_expr_words = r'(\w)+'
     return len(re.findall(reg_expr_letters, text)) / len(re.findall(reg_expr_words, text))
+
+#def count_n_grams():

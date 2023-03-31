e779a6650851183f19c4bd545376776631e3edab
Oleg-Dainovich <olegdainovich@gmail.com>
2023-03-31 03:44:44 +0300

    add abbreviations in text statistics

--- lab-02/text-statistics/src/functions.py
+++ lab-02/text-statistics/src/functions.py
@@ -1,9 +1,30 @@
 import re
 
+ABBREVIATIONS = (
+    'etc.', 'vs.', 'a.d.', 'b.c.', 'a.m.', 'p.m.', 'jr.',
+    'sr.', 'v.i.p.', 'p.s.', 'p.p.s.', 'smth.', 'smb.', 'yr.',
+    'jan.', 'feb.', 'mar.', 'jun.', 'jul.', 'aug.', 'sep.',
+    'oct.', 'nov.', 'dec.', 'mon.', 'tue.', 'wed.', 'thu.',
+    'fri.', 'sat.', 'sun.', 'mr.', 'mrs.', 'ms.', 'dr.',
+    'st.', 'inc.', 'corp.'  
+)
+
+def replace_abbreviations(text: str):
+    new_text = text
+    for abbreviation in ABBREVIATIONS:
+        #reg_expr = r"abbreviation"
+        new_text = re.sub(abbreviation, 'word', new_text)
+    return new_text
+
 def count_sentences(text: str):
+    new_text = replace_abbreviations(text)
     reg_expr = r'(\.|\?|!)+'
-    return len(re.findall(reg_expr, text))
+    print(new_text)
+    return len(re.findall(reg_expr, new_text))
 
 def count_non_declarative_sentences(text: str):
     reg_expr = r'\.+'
     return count_sentences(text) - len(re.findall(reg_expr, text))
+
+#def count_length_of_sentence(text: str):
+

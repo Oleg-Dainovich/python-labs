870e52c3592c1c92a00e10f1ba71202e6584498a
Oleg-Dainovich <olegdainovich@gmail.com>
2023-04-14 02:43:11 +0300

    add n-grams in text statistics

--- lab-02/text-statistics/src/functions.py
+++ lab-02/text-statistics/src/functions.py
@@ -1,4 +1,6 @@
 import re
+#from functools import reduce
+from collections import Counter
 
 ABBREVIATIONS = (
     'etc.', 'vs.', 'a.d.', 'b.c.', 'a.m.', 'p.m.', 'jr.',
@@ -16,7 +18,7 @@ def replace_abbreviations(text: str):
     return new_text
 
 def find_words(text: str):
-    words_list = re.findall(r'\w*[a-zA-Z]\w*', text)
+    words_list = re.findall(r'\w*[a-zA-Z]\w*', text)                                    #remove numbers and punctuation
     new_text = " ".join(words_list)
     return new_text
 
@@ -31,20 +33,39 @@ def count_non_declarative_sentences(text: str):
     return count_sentences(text) - len(re.findall(reg_expr, new_text))
  
 def count_length_of_sentences(text: str):
-    new_text = replace_abbreviations(text)
-    print(new_text)
-    new_text = find_words(new_text)
-    print(new_text)
+    #new_text = replace_abbreviations(text)
+    #print(new_text)
+    new_text = find_words(text)
+    #print(new_text)
     reg_expr = r'(\w)+'
     return len(re.findall(reg_expr, new_text)) / count_sentences(text)
 
 def count_length_of_words(text: str):
-    new_text = replace_abbreviations(text)
-    new_text = find_words(new_text)
+    #new_text = replace_abbreviations(text)
+    new_text = find_words(text)
     reg_expr_letters = r'\w'
     reg_expr_words = r'(\w)+'
-    return len(re.findall(reg_expr_letters, text)) / len(re.findall(reg_expr_words, text))
+    return len(re.findall(reg_expr_letters, new_text)) / len(re.findall(reg_expr_words, new_text))
 
-#def count_n_grams():
+def get_n_grams(text: str, n: int):
+    words_list = re.findall(r'\w*[a-zA-Z]\w*', text)
+    n_grams_list: list[str] = []
+    for word in words_list:
+        if len(word) >= n:
+            for i in range(0, len(word) - n + 1):
+                #reduce(lambda x, y: x + y, word[i: i + n])
+                n_grams_list.append("".join(word[i: i + n]))
+    print(n_grams_list)
+    return n_grams_list
 
-#    return
+def get_top_k_n_grams(text: str, k: int, n: int):
+    n_grams_list = get_n_grams(text, n)
+    top_k_n_grams = Counter(n_grams_list).most_common(k)
+    return top_k_n_grams
+
+def print_top_k_n_grams(text: str, k: int = 10, n: int = 4):
+    top_k_n_grams = get_top_k_n_grams(text, k, n)
+    print("Top", k, "N-grams:")
+    for n_gram, amount in top_k_n_grams:
+        print("N-gram:", n_gram, "|", amount, "appearance(s)")
+    return
--- lab-02/text-statistics/src/main.py
+++ lab-02/text-statistics/src/main.py
@@ -2,12 +2,17 @@ from functions import (
     count_sentences, 
     count_non_declarative_sentences, 
     count_length_of_sentences,
-    count_length_of_words
+    count_length_of_words,
+    print_top_k_n_grams
 )
 
 text = input('Enter the text: ')
+text = text.lower()
 
-print('Amount of sentences in the text: ', count_sentences(text))
-print('Amount of non-declarative sentences in the text: ', count_non_declarative_sentences(text))
-print('Average length of the sentence: ', count_length_of_sentences(text))
-print('Average length of the word: ', count_length_of_words(text))
+print('Amount of sentences in the text:', count_sentences(text))
+print('Amount of non-declarative sentences in the text:', count_non_declarative_sentences(text))
+print('Average length of the sentence:', count_length_of_sentences(text))
+print('Average length of the word:', count_length_of_words(text))
+
+k, n = map(int, input('Enter K and N for N-grams: ').split())
+print_top_k_n_grams(text, k, n)

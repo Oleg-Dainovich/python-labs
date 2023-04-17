a6e97b649dc9eeef6d2d9b2a0237cc6dd7f4932b
Oleg-Dainovich <olegdainovich@gmail.com>
2023-04-18 01:04:40 +0300

    add container class

---
+++ lab_02/unique_container/src/container_class.py
@@ -0,0 +1,28 @@
+class UniqueContainer:
+    __username: str
+    __container: set[str]
+    __filename: str
+
+    def __init__(self):
+        pass
+
+    def add(self, key: str):                            # user need to be able to input several keys 
+        pass
+
+    def remove(self, key: str):
+        pass
+
+    def find(self, key:str):                            # user need to be able to input several keys
+        pass
+
+    def list(self):
+        pass
+
+    def grep(self, regex):
+        pass
+
+    def save(self):
+        pass
+
+    def load(self):
+        pass

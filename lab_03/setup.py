8bedafe0376b343a8adef49608013f5aeb544517
Oleg-Dainovich <olegdainovich@gmail.com>
2023-05-23 02:30:38 +0300

    add setup file

---
+++ lab_03/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup (
+    name="oleg-custom-serializer",
+    version="0.1.0",
+    packages=[
+        "src",
+        "src.encoder",
+        "src.serializers"
+    ],
+
+    url="https://www.youtube.com/watch?v=dQw4w9WgXcQ",
+    license="",
+    author="Oleg-Dainovich",
+    author_email="",
+    description="",
+)

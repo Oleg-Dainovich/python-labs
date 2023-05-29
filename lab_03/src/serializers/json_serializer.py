9308f7c72a90127454205063b2aa705f429a9803
Oleg-Dainovich <olegdainovich@gmail.com>
2023-05-29 22:00:00 +0530

    refactoring

--- lab_03/src/serializers/json_serializer.py
+++ lab_03/src/serializers/json_serializer.py
@@ -1,135 +1,132 @@
-from typing import IO
-
-from .base_serializer import BaseSerializer
+from ..encoder.encoder import Encoder, Decoder
 
 
-class JsonSerializer(BaseSerializer):
-    @classmethod
-    def dumps(cls, obj) -> str:
-        if isinstance(obj, bool):
-            return str(obj).lower()
+def dump(obj, file):
+    file.write(dumps(obj))
 
-        if isinstance(obj, (int, float)):
-            return str(obj)
 
-        if isinstance(obj, str):
-            return f'"{obj}"'
+def dumps(obj):
+    data = Encoder.encode(obj)
+    return _dumps(data)
 
-        if isinstance(obj, type(None)):
-            return "null"
 
-        if isinstance(obj, (list, tuple)):
-            return f"[{','.join(list(map(cls.dumps, obj)))}]"
+def _dumps(obj):
+    if isinstance(obj, (int, float)):
+        return str(obj)
+    if isinstance(obj, bool):
+        return str(obj).lower()
+    if isinstance(obj, str):
+        return f'"{str(obj)}"'
+    if isinstance(obj, type(None)):
+        return "null"
+    if isinstance(obj, (list, tuple)):
+        return f"[{', '.join(list(map(_dumps, obj)))}]"
+    if isinstance(obj, dict):
+        data = ", ".join([f'{_dumps(key)}: {_dumps(value)}' for key, value in obj.items()])
+        return f"""{{{data}}}"""
 
-        if isinstance(obj, dict):
-            data = ",".join(
-                [f'"{key}": {cls.dumps(value)}' for (key, value) in obj.items()]
-            )
-            return f"""{{{data}}}"""
 
-    @classmethod
-    def dump(cls, obj, fp: IO[str]) -> None:
-        fp.write(cls.dumps(obj))
+def loads(obj):
+    res, _ = _loads(obj, 0)
+    return Decoder.decode(res)
 
-    @classmethod
-    def loads(cls, s: str):
-        res, _ = cls._loads(s, 0)
-        return res
 
-    @classmethod
-    def load(cls, fp: IO[str]):
-        return cls.loads(fp.read())
+def load(file):
+    return loads(file.read())
 
-    @classmethod
-    def _loads(
-        cls, s: str, start_index: int
-    ) -> tuple[bool | str | int | float | list | dict | None, int]:
-        if s[start_index] == '"':
-            return cls._loads_str(s, start_index)
-        if s[start_index] == "[":
-            return cls._loads_list(s, start_index)
 
-        if s[start_index].isdigit():
-            return cls._loads_num(s, start_index)
+def _loads(obj: str, start_index):
+    if obj[start_index] == '{':
+        return _loads_dict(obj, start_index)
+    if obj[start_index] == '"':
+        return _loads_str(obj, start_index)
+    if obj[start_index] == '[':
+        return _loads_list(obj, start_index)
+    if obj[start_index].isdigit() or obj[start_index] == '-':
+        return _loads_num(obj, start_index)
+    if obj[start_index] == 'n':
+        return None, start_index + 4
+    if obj[start_index] == 't':
+        return True, start_index + 4
+    if obj[start_index] == 'f':
+        return False, start_index + 5
+    if obj[start_index].isalpha():
+        return _loads_str_type(obj, start_index)
 
-        if s[start_index] == "t":
-            return True, start_index + 4
 
-        if s[start_index] == "f":
-            return False, start_index + 5
+def _loads_str_type(obj, start_index):
+    end = start_index + 1
+    while end < len(obj) and obj[end] not in [',', ']', '}']:
+        end += 1
+    return obj[start_index:end], end + 1
 
-        if s[start_index] == "n":
-            return None, start_index + 4
 
-        if s[start_index] == "{":
-            return cls._loads_dict(s, start_index)
+def _loads_num(obj, start_index):
+    end = start_index + 1
 
-    @classmethod
-    def _loads_list(cls, s: str, start_index: int) -> tuple[list, int]:
-        end_index = start_index + 1
-        braces = 1
-        while braces:
-            if s[end_index] == "[":
-                braces += 1
-            if s[end_index] == "]":
-                braces -= 1
+    while end < len(obj) and (obj[end].isdigit() or obj[end] == '.'):
+        end += 1
 
-            end_index += 1
+    str_num = obj[start_index:end]
+    num = 0
+    try:
+        num = int(str_num)
+    except ValueError:
+        num = float(str_num)
+    finally:
+        return num, end + 1
 
-        arr = []
-        index = start_index + 1
-        while index < end_index - 2:
-            while s[index] in (" ", ",", "\n"):
-                index += 1
-            res, index = cls._loads(s, index)
-            arr.append(res)
 
-        return arr, end_index
+def _loads_str(obj, start_index):
+    end = start_index + 1
 
-    @classmethod
-    def _loads_dict(cls, s: str, start_index: int) -> tuple[dict, int]:
-        end_index = start_index + 1
-        braces = 1
-        while braces:
-            if s[end_index] == "{":
-                braces += 1
-            if s[end_index] == "}":
-                braces -= 1
+    while obj[end] != '"':
+        end += 1
 
-            end_index += 1
+    return obj[start_index + 1:end], end + 1
 
-        index = start_index + 1
-        result = {}
-        while index < end_index - 2:
-            while s[index] in (" ", ",", "\n"):
-                index += 1
-            key, index = cls._loads_str(s, index)
 
-            while s[index] in (" ", ",", "\n", ":"):
-                index += 1
+def _loads_dict(obj, start_index):
+    end = start_index + 1
+    brackets = 1
 
-            value, index = cls._loads(s, index)
-            result[key] = value
+    while brackets:
+        if obj[end] == '{':
+            brackets += 1
+        elif obj[end] == '}':
+            brackets -= 1
+        end += 1
 
-        return result, end_index + 1
+    dct = {}
+    i = start_index + 1
+    while i < end - 1:
+        while obj[i] in [' ', ',', '\n']:
+            i += 1
+        key, i = _loads(obj, i)
+        while obj[i] in [' ', '\n', ',', ':']:
+            i += 1
+        value, i = _loads(obj, i)
+        dct[key] = value
 
-    @classmethod
-    def _loads_str(cls, s: str, start_index: int) -> tuple[str, int]:
-        end_index = start_index + 1
-        while s[end_index] != '"':
-            end_index += 1
+    return dct, end + 1
 
-        return s[start_index + 1: end_index], end_index + 1
 
-    @classmethod
-    def _loads_num(cls, s: str, start_index: int) -> tuple[int | float, int]:
-        end_index = start_index + 1
-        while len(s) > end_index and (s[end_index].isdigit() or s[end_index] == "."):
-            end_index += 1
+def _loads_list(obj, start_index):
+    end = start_index + 1
+    brackets = 1
+    while brackets:
+        if obj[end] == '[':
+            brackets += 1
+        elif obj[end] == ']':
+            brackets -= 1
+        end += 1
 
-        num = s[start_index:end_index]
+    lst = []
+    i = start_index + 1
+    while i < end - 1:
+        while obj[i] in [' ', ',', '\n']:
+            i += 1
+        res, i = _loads(obj, i)
+        lst.append(res)
 
-        if num.count("."):
-            return float(num), end_index
-
-        return int(num), end_index
+    return lst, end + 1
